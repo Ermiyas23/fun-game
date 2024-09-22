2599c4f0de5e23cbe06bfdc86c99a8fe14a28b80
Ermiyas23 <ermi23@on.com>
2024-09-22 07:59:39 -0700

    Creat rondom computer choice

--- js/script1.js
+++ js/script1.js
@@ -1,2 +1,11 @@
-console.log('hey,ermi');
-console.log('what it is wrong!')
+console.log('let try something ermi');
+function getComputerChoice(arr){
+    // get random index from array
+    const Random = Math.floor(Math.random() * arr.length);
+    // get random item
+    const randomItem =arr[Random];
+    return randomItem;
+}
+const array =['rock','paper','scissor'];
+const result = getComputerChoice(array);
+console.log(result);

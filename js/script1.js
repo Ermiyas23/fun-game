3d454230fd221c8d38ea507653cea015ce12f267
Ermiyas23 <ermi23@on.com>
2024-09-25 06:11:55 -0700

    Create play round fun

--- js/script1.js
+++ js/script1.js
@@ -1,9 +1,9 @@
 console.log('let try something ermi');
-function getComputerChoice(arr){
+function getComputerChoice(array){
     // get random index from array
-    const Random = Math.floor(Math.random() * arr.length);
+    const Random = Math.floor(Math.random() * array.length);
     // get random item
-    const randomItem =arr[Random];
+    const randomItem =array[Random];
     return randomItem;
 }
 const array =['rock','paper','scissor'];
@@ -11,12 +11,55 @@ const result = getComputerChoice(array);
 console.log(result);
 
 // getting input from user
-const userInput = prompt(`write one of them , rock ,paper,scissor`,'');
+const userInput = prompt(`write one of them , rock ,paper,scissor`);
 
 function getHumanChoice(Input){
 return userInput;
 }
 let showInput = getHumanChoice(userInput);
-console.log(showInput);
+// console.log(showInput);
 
+// count human and computer score 
+let humanScore = 0;
+let computerScore = 0;
 
+// cerate single round 
+function playRound(computerChoice,humanChoice){
+    //....play
+    if (computerChoice === 'rock'){
+        if (humanChoice == 'paper'){
+            console.log(`your choice is ${humanChoice} and you win`);
+            humanScore ++;
+
+        }else if (humanChoice == 'scissor'){
+            console.log(`your choice is ${humanChoice} and you loss`);
+            computerScore ++;
+        }
+        else {console.log('try again')}
+    }
+    else if (computerChoice === 'paper'){
+        if (humanChoice == 'rock'){
+            console.log(`your choice is ${humanChoice} and you win`);
+            humanScore ++;
+        }else if (humanChoice == 'scissor'){
+            console.log(`your choice is ${humanChoice} and you loss`);
+            computerScore ++;
+        }else{console.log('try again')}
+ 
+    }
+    else if (computerChoice === 'scissor'){
+        if (humanChoice == 'paper'){
+            console.log(`your choice is ${humanChoice} and you loss`);
+            humanScore ++;
+        }else if (humanChoice == 'rock'){
+            console.log(`your choice is ${humanChoice} and you win`);
+            computerScore ++;
+        }else {console.log('try again')}
+    }
+
+}
+
+const computerSelection  = getComputerChoice(array);
+const humanSelection  = getHumanChoice(userInput);
+
+playRound(computerSelection,humanSelection);
